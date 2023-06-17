2c95d2fe08c2ac74e38653eb135f2ff7293ccbd2
LightningLion_Student <662781@student.inholland.nl>
2023-06-17 19:30:16 +0200

    Progress
    - Removed dummy knowledge graph
    - Renamed some variables
    - Added randomly chosen negative response

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,17 +1,17 @@
 import spacy
 import json
+import random
 from services.fuzzy_matcher import FuzzyMatcher
 from services.kggenerator import KGGenerator
 
 class ChatBot:
 
     def __init__(self) -> None:
-        self.dummy_knowledge_graph = {
-            "apple": "A fruit that is red or green in color and grows on trees.",
-            "banana": "A fruit that is yellow and grows on trees.",
-            "orange": "A citrus fruit that is typically orange in color.",
-            "watermelon": "A large fruit with green skin and red flesh."
-        }
+        self.negative_responses = [
+            "I don't know.",
+            "I couldn't find the answer, please formulate your question differently.",
+            "I don't know this. Feel free to ask another question."
+        ]
         self.fuzzmatch = FuzzyMatcher()
         self.nlp = spacy.load('en_core_web_sm')
 
@@ -19,10 +19,11 @@ class ChatBot:
         return self.fuzzmatch.find_answer(user_input)
     
     def respond(self, user_input):
-        information = self.process_input(user_input)
+        answer = self.process_input(user_input)
 
-        if information:
-            return information
+        if answer:
+            return answer
         else:
-            return "I don't know."
+            # Return random negative response 
+            return random.choice(self.negative_responses)
 
